6ef94fff496d10dc392c73541d69801c921c21d0
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-09-29 18:28:32 +0530

    update : background

--- js/app.js
+++ js/app.js
@@ -1,58 +1,116 @@
 particlesJS('particles-js', {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: '#ffffff' },
+    number: {
+      value: 500,
+      density: {
+        enable: true,
+        value_area: 889.1476416322727
+      }
+    },
+    color: {
+      value: '#4DD637'
+    },
     shape: {
-      type: 'circle',
-      stroke: { width: 0, color: '#000000' },
-      polygon: { nb_sides: 5 },
-      image: { src: 'img/github.svg', width: 100, height: 100 }
+      type: 'star',
+      stroke: {
+        width: .2,
+        color: '#DDD101'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100
+      }
     },
     opacity: {
-      value: 0.5,
+      value: 0.48927153781200905,
       random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0,
+        sync: false
+      }
     },
     size: {
-      value: 3,
+      value: 4,
       random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      anim: {
+        enable: true,
+        speed: 8,
+        size_min: 0,
+        sync: false
+      }
     },
     line_linked: {
-      enable: true,
+      enable: false,
       distance: 150,
       color: '#ffffff',
-      opacity: 0.4,
+      opacity: 0.2,
       width: 1
     },
     move: {
       enable: true,
-      speed: 6,
+      speed: 0.9,
       direction: 'none',
-      random: false,
+      random: true,
       straight: false,
       out_mode: 'out',
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      attract: {
+        enable: false,
+        rotateX: 650,
+        rotateY: 1200
+      }
     }
   },
   interactivity: {
-    detect_on: 'canvas',
+    detect_on: 'window',
     events: {
-      onhover: { enable: true, mode: 'repulse' },
-      onclick: { enable: true, mode: 'push' },
+      onhover: {
+        enable: true,
+        mode: 'bubble'
+      },
+      onclick: {
+        enable: true,
+        mode: 'push'
+      },
       resize: true
     },
     modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 }
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 183.91608391608392,
+        size: 1.5,
+        duration: 2,
+        opacity: 1,
+        speed: 6
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
     }
   },
   retina_detect: true
 })
+
+/* ---- stats.js config ---- */
+
 var count_particles, stats, update
 stats = new Stats()
 stats.setMode(0)
